e4941bc1895df7daf7fb139ea65c31e6635ac0c6
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-03-09 11:11:52 +0700

    resolve page project h5

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -7,7 +7,7 @@ export default function page() {
     return (
         <main>
             <h1 className='font-bold text-3xl lg:text-5xl mb-3 uppercase'>what project we do? </h1>
-            <h5 className='text-sm text-primary/50 lg:text-lg md:w-3/4 lg:w-2/3'>The projects we work on aim to fulfill your desires in achieving common goals. Every product we create has a special story. Our role as a technology partner is to help overcome obstacles and stimulate our clients' growth.</h5>
+            <h5 className='text-sm text-primary/50 lg:text-lg md:w-3/4 lg:w-2/3'>The projects we work on aim to fulfill your desires in achieving common goals. Every product we create has a special story. Our role as a technology partner is to help overcome obstacles and stimulate our clients growth.</h5>
             <div className='mt-10 space-y-10'>
                 <UiUx />
                 <Mobile />
